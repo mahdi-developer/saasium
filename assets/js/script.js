8802d061453cfbba40cb0ace12c6230f7a5eff81
Mahdi <muhammadmahdiulislam13@gmail.com>
2023-12-20 18:51:17 +0600

    second

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@
   splide.mount();
 
 
-  let splide1 = new Splide(".splide1", {
+  var splide1 = new Splide("#testimonial", {
     type: "loop",
     perPage: 3,
     perMove: 1,
@@ -45,3 +45,12 @@
   });
 
   splide1.mount();
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".top").offset().top},"1000");return false})})
