aae3000e39803721e78c79b765f9afa1cf685668
Mahdi <muhammadmahdiulislam13@gmail.com>
2024-01-03 19:08:44 +0600

    Package

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,6 +143,27 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// Package Toggler
+function check() {
+  var checkBox = document.getElementById("checbox");
+  var text1 = document.getElementsByClassName("text1");
+  var text2 = document.getElementsByClassName("text2");
+
+  for (var i = 0; i < text2.length; i++) {
+    if (checkBox.checked == true) {
+      text1[i].style.display = "none";
+      text2[i].style.display = "block";
+    } else if (checkBox.checked == false) {
+      text1[i].style.display = "block";
+      text2[i].style.display = "none";
+    }
+  }
+}
+check();
+
+
+
+// Scroll Top
   $(window).scroll(function() {
     if ($(this).scrollTop() > 50 ) {
         $('.scrolltop:hidden').stop(true, true).fadeIn();
